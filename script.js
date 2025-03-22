92511778eb2e8a6d573e04f05a4680131d7ea1ce
Szidelo <claudiuszidelo@yahoo.ro>
2025-03-22 12:54:03 +0200

    game logic done. game can be played in console. it handles checking tie for each player, switch player every round, handle a winner if a winning patter exist, can reset the game

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // functionality first, UI second with separate factory functions
 // logic and UI will be separated into separate factory functions
 
-const GameBoard = () => {
-	let size = 3; // initial size of the board
+const GameBoard = (gridSize = 3) => {
+	let size = gridSize;
 	let board = [];
 
 	const initBoard = () => {
@@ -27,20 +27,19 @@ const GameBoard = () => {
 	const getBoard = () => board;
 
 	const isCellAvailable = (row, col) => {
-		return board[row][col].getValue().char === "*"; // to be changed
+		return board[row][col].getValue().playerName === null; // to be changed
 	};
 
 	const setCell = (row, col, char, player) => {
 		// change to pass row col and a player object argument
 		if (isCellAvailable(row, col)) {
 			board[row][col].setValue(char, player); //player object argument
-			console.log(`cell at (${row}, ${col}) has been claimed by ${player} as '${char}'`);
 		} else {
 			console.warn(`cell at row ${row} and col ${col} is not available`);
 		}
 	};
 
-	// TODO: add a checkTie method that check that there are no cells === "*"
+	const checkTie = () => board.flat().every((cell) => cell.getValue().playerName !== null);
 
 	const generateWinPatterns = () => {
 		const rows = [];
@@ -77,24 +76,23 @@ const GameBoard = () => {
 
 	const printConsoleTable = () => {
 		const boardValues = board.map((row) => row.map((cell) => cell.getValue().char));
-
 		console.table(boardValues);
 	};
 
-	return { getBoard, printConsoleTable, isCellAvailable, setCell, checkWinner, resetBoard, generateWinPatterns };
+	return { getBoard, printConsoleTable, isCellAvailable, setCell, checkWinner, resetBoard, generateWinPatterns, checkTie };
 };
 
 const Cell = () => {
 	const value = {
 		char: "*",
-		player: null,
+		playerName: null,
 	};
 
 	const getValue = () => value;
 
 	const setValue = (char, player) => {
 		value.char = char;
-		value.player = player;
+		value.playerName = player;
 	};
 
 	return { getValue, setValue };
@@ -130,6 +128,7 @@ const GameController = (playerOneName = "Player One", playerTwoName = "Player Tw
 	const switchPlayer = () => {
 		activePlayerIndex = 1 - activePlayerIndex; // toggle index
 		rounds++;
+		printRound();
 		console.log("+++game round:", rounds);
 	};
 
@@ -143,12 +142,6 @@ const GameController = (playerOneName = "Player One", playerTwoName = "Player Tw
 
 	const playRound = (row, col) => {
 		const { char, playerName } = getActivePlayer();
-		if (rounds >= 9) {
-			console.warn("No more possible moves!");
-			gameEnded = true;
-			resetGame();
-			return;
-		}
 
 		if (gameEnded) {
 			console.warn("Game has ended! No more moves allowed.");
@@ -167,13 +160,17 @@ const GameController = (playerOneName = "Player One", playerTwoName = "Player Tw
 		board.setCell(row, col, char, playerName);
 		const winner = board.checkWinner(char);
 		if (winner) {
+			printRound();
 			console.log("!! And the winner is:", playerName, "!!");
 			gameEnded = true;
-			printRound();
 			return;
 		} else {
-			printRound();
 			switchPlayer();
+		}
+		if (board.checkTie()) {
+			console.warn(`game ended in a tie, rounds played: ${rounds}`);
+			gameEnded = true;
+			return;
 		}
 	};
 
@@ -182,7 +179,7 @@ const GameController = (playerOneName = "Player One", playerTwoName = "Player Tw
 	return { playRound, getActivePlayer, resetGame };
 };
 
-console.log("--------------test 1----------------");
+console.log("--------------test 1 3x3 grid attempt to check cell after game is won----------------");
 const game = GameController();
 game.playRound(0, 0);
 game.playRound(0, 1);
@@ -193,3 +190,18 @@ game.playRound(1, 2);
 game.playRound(2, 0);
 game.playRound(2, 2);
 game.playRound(2, 1);
+game.playRound(1, 1);
+game.playRound(2, 2);
+
+console.log("--------------test 1 3x3 grid check tie if all cells are checked----------------");
+
+const game2 = GameController();
+game2.playRound(0, 0); // X
+game2.playRound(0, 1); // O
+game2.playRound(0, 2); // X
+game2.playRound(1, 1); // O
+game2.playRound(1, 0); // X
+game2.playRound(1, 2); // O
+game2.playRound(2, 1); // X
+game2.playRound(2, 0); // O
+game2.playRound(2, 2); // X
